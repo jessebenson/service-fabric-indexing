e1b4406762970179e424ac944e130a3cc44e9c6f
jessebenson <jesse.benson@microsoft.com>
2017-08-17 16:27:26 -0700

    Add empty sample application for Memory indexing.

---
+++ samples/Memory/StoreService/StoreService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+namespace StoreService
+{
+	/// <summary>
+	/// An instance of this class is created for each service replica by the Service Fabric runtime.
+	/// </summary>
+	internal sealed class StoreService : StatefulService
+	{
+		public StoreService(StatefulServiceContext context)
+			: base(context)
+		{ }
+
+		/// <summary>
+		/// This is the main entry point for your service replica.
+		/// This method executes when this replica of your service becomes primary and has write status.
+		/// </summary>
+		/// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+		protected override async Task RunAsync(CancellationToken cancellationToken)
+		{
+			var products = await StateManager.GetOrAddAsync<IReliableDictionary<string, Product>>("products");
+		}
+	}
+}
