59ebaa9b9caa93e50590b7caf354f763d4dab2c1
jessebenson <jesse.benson@microsoft.com>
2017-08-17 16:00:34 -0700

    Add empty in-memory indexing library.

---
+++ src/Microsoft.ServiceFabric.Data.Indexing.Memory.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microsoft.ServiceFabric.Data.Indexing.Memory.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.ServiceFabric.Data.Indexing.Memory.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed08dbc1-203e-4645-9395-dd4554265e3b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
