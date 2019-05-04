8bb9fbd5cb03b5b715091d3a5a3d921e901cd86c
Wayne Munro <wamunro@microsoft.com>
2019-05-04 11:08:36 +0200

    fixed failing unit test StringFilter_Add_WithNullString

--- src/ServiceFabric.Extensions.Data.Indexing.Test/FilterableIndexTests.cs
+++ src/ServiceFabric.Extensions.Data.Indexing.Test/FilterableIndexTests.cs
@@ -43,10 +43,10 @@ namespace ServiceFabric.Extensions.Data.Indexing.Persistent.Test
                 // Search the index for the wrong name.  This should not return any results.
                 temp = await dictionary.FilterAsync<string>(tx, "name", null);
                 var nobody = new List<KeyValuePair<Guid, Person>>(await temp.ToEnumerable());
-                Assert.AreEqual(1, nobody.Count());
-                Assert.AreEqual(noname.Id, result.First().Key);
-                Assert.AreEqual(-1, result.First().Value.Age);
 
+                Assert.AreEqual(1, nobody.Count());
+                Assert.AreEqual(noname.Id, nobody.First().Key);
+                Assert.AreEqual(-1, nobody.First().Value.Age);
 
                 await tx.CommitAsync();
             }
